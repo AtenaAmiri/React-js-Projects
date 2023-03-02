6f328895846b7ef73d1421f60be1823ae12ccbc1
atena <ftemeh.amiirii@gmail.com>
2023-03-02 21:28:03 +0330

    1

---
+++ robofriends/src/Component/Hello.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../Style/Hello.css";
+export default class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <p>Welcome to React</p>
+      </div>
+    );
+  }
+}
