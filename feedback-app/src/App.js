fc6eb91f64e418121784c610ee177b555debc55f
atena <ftemeh.amiirii@gmail.com>
2023-03-03 22:06:03 +0330

    feedback

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -4,6 +4,7 @@ import Header from "./component/Header";
 import propTypes from "prop-types";
 import FeedbackList from "./component/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./component/FeedbackStats";
 
 export default function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -17,6 +18,7 @@ export default function App() {
     <>
       <Header />
       <div className="container">
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
